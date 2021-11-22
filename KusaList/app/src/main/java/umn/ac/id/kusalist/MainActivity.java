003a8ce1ef53bc79c6e628c7c1c41920272d5f9c
rafi365 <74255365+rafi365@users.noreply.github.com>
2021-11-22 12:02:53 +0700

    added FAB add list pop up dialog box

--- KusaList/app/src/main/java/umn/ac/id/kusalist/MainActivity.java
+++ KusaList/app/src/main/java/umn/ac/id/kusalist/MainActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -19,16 +24,16 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView iniRv;
     listAdapter rvAdapter;
     LinkedList<NoteArray> daftarNama = new LinkedList<>();
-
+    final Context c = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("KusaList");
 
-        for(int i = 0 ; i < 10 ; i++){
-            daftarNama.add(new NoteArray("example"+ (i+1),"this is some text and stuff\n hellow world!!!\n number :"+(i+1)+"\n checkbox : "+((i % 2) == 0),null,(((i % 2) == 0))));
-        }
+//        for(int i = 0 ; i < 10 ; i++){
+//            daftarNama.add(new NoteArray("example"+ (i+1),"this is some text and stuff\n hellow world!!!\n number :"+(i+1)+"\n checkbox : "+((i % 2) == 0),null,(((i % 2) == 0))));
+//        }
 
         iniRv = findViewById(R.id.iniRv);
         rvAdapter = new listAdapter(this, daftarNama);
@@ -38,8 +43,35 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
+                View mView = layoutInflaterAndroid.inflate(R.layout.user_input_dialog_box, null);
+                AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
+                alertDialogBuilderUserInput.setView(mView);
+
+                final EditText userInputDialogEditText = (EditText) mView.findViewById(R.id.titleInputDialog);
+                alertDialogBuilderUserInput
+                        .setCancelable(false)
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                // ToDo get user input here
+                                if( userInputDialogEditText.getText().toString().isEmpty()){
+                                    Toast.makeText(c, "List title cannot be empty!", Toast.LENGTH_LONG).show();
+                                }else{
+                                    daftarNama.add(new NoteArray(userInputDialogEditText.getText().toString(),"this is some text and stuff\n hellow world!!!",null,false));
+                                    rvAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        })
+
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialogBox, int id) {
+                                        dialogBox.cancel();
+                                    }
+                                });
+
+                AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
+                alertDialogAndroid.show();
             }
         });
     }
